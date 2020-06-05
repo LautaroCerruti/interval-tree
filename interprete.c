d1c10a717e7106cf9b2e75e6b5ebbeb1511fe941
Marcos Cassinerio <marcos.cassinerio@mangoodev.com>
2020-06-05 00:13:28 -0300

    fixed interprete

--- interprete.c
+++ interprete.c
@@ -2,53 +2,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_LINEA 250
+#include <ctype.h>
+#define MAX_LINEA 658
+
+char *leer_cadena(char *string) {
+    char c, *aux = string;
+    while ((c = getchar()) != '\n') {
+        if (c != '\r') {
+            *string = c;
+            ++string;
+        }
+    }
+    *string = '\0';
+    return aux;
+}
+
+char obtener_operacion(char *string, Intervalo **intervalo) {
+    char operacion = string[0], *resto;
+    double numero1, numero2;
+    if (strlen(string) >= 8 && string[1] == ' ' && string[2] == '[') {
+        string += 3;
+        if (isdigit(*string) != 0) {
+            numero1 = strtod(string, &resto);
+        }
+        if (*resto == ',' && *(resto + 1) == ' ' && isdigit(*(resto + 2))) {
+            string = resto + 2;
+            numero2 = strtod(string, &resto);
+        }
+        if (*resto == ']' && *(resto + 1) == '\0') {
+            *intervalo = intervalo_crear(numero1, numero2);
+            return operacion;
+        }
+    }
+    return '\0';
+}
 
 int main() {
-    char buffer[MAX_LINEA], operacion;
-    double fn, sn;
+    char buffer[MAX_LINEA];
     buffer[0] = '\0';
     ITree arbol = itree_crear(), nodo;
     Intervalo *intervalo;
 
-    while(strcmp(fgets(buffer, sizeof(buffer), stdin), "salir\0")){
-        if(sscanf(buffer, "%c [%lf, %lf]", &operacion, &fn, &sn) == 3){
-            switch (operacion)
-            {
-            case 'i':
-                intervalo = intervalo_crear(fn, sn);
-                arbol = itree_insertar(arbol, intervalo);
-                break;
-            case 'e':
-                intervalo = intervalo_crear(fn, sn);
-                arbol = itree_eliminar(arbol, intervalo);
-                free(intervalo);
-                break;
-            case '?':
-                intervalo = intervalo_crear(fn, sn);
-                nodo = itree_intersecar(arbol, intervalo);
-                if (nodo) {
-                    printf("Si ");
-                    itree_imprimir_intervalo(arbol, intervalo_imprimir);
-                } else {
-                    printf("No");
-                }
-                nodo = NULL;
-                break;
-            default:
-                printf("Comando Incorrecto");
-                break;
-            }
-            printf("\n\n");
+    while(strcmp(leer_cadena(buffer), "salir\0")) {
+        if (!strcmp(buffer, "dfs\0")) {
+            itree_recorrer_dfs(arbol, intervalo_imprimir);
         } else if (!strcmp(buffer, "bfs\0")) {
             itree_recorrer_bfs(arbol, intervalo_imprimir);
-            printf("\n\n");
-        } else if (!strcmp(buffer, "dfs\0")) {
-            itree_recorrer_dfs(arbol, intervalo_imprimir);
-            printf("\n\n");
         } else {
-            printf("Comando Incorrecto\n\n");
+            switch (obtener_operacion(buffer, &intervalo)) {
+                case 'i':
+                    arbol = itree_insertar(arbol, intervalo);
+                    break;
+                case 'e':
+                    arbol = itree_eliminar(arbol, intervalo);
+                    free(intervalo);
+                    break;
+                case '?':
+                    nodo = itree_intersecar(arbol, intervalo);
+                    if (nodo) {
+                        printf("Si ");
+                        itree_imprimir_intervalo(nodo, intervalo_imprimir);
+                    } else {
+                        printf("No");
+                    }
+                    printf("\n");
+                    nodo = NULL;
+                    break;
+                default:
+                    printf("Comando Incorrecto");
+                    break;
+            }
         }
+        printf("\n\n");
     }
     itree_destruir(arbol, intervalo_destruir);
     return 0;
